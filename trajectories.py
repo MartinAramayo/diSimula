✨ Add config for correct vector scaling

trajectories.py
@@ -29,16 +29,18 @@ def parabolic_trajectory(x0, y0, v0, angle, g, dt):
 def parabolic_trajectory_multi(x0, y0, v0, angle, g, dt):
     
     R_vector = []
-    for n_object in range(72):
 
+    
+    # Calculate time of flight
+    t_max = 2 * v0 * np.sin(45) / g
+
+    # Create time array
+    t = np.arange(0, 2*t_max, dt)
+    
+    for n_object in range(72):
+    
         # Convert angle to radians
         theta = np.radians(angle + 5 * n_object)
-        
-        # Calculate time of flight
-        t_max = 2 * v0 * np.sin(45) / g
-
-        # Create time array
-        t = np.arange(0, 2*t_max, dt)
 
         # Calculate x and y positions
         x = x0 + v0 * np.cos(theta) * t
@@ -51,7 +53,7 @@ def parabolic_trajectory_multi(x0, y0, v0, angle, g, dt):
 
     return pack_object_multi(R_vector)
 
-def MRU(x0, y0, v0, angle, dt, t_max):
+def MRU(x0, y0, v0, angle, R, dt, t_max):
     
     # Convert angle to radians
     theta = np.radians(angle)
@@ -67,6 +69,28 @@ def MRU(x0, y0, v0, angle, dt, t_max):
     r_vector = np.asarray((x, y)).T
 
     return pack_object(r_vector)
+
+def MCU_multi(x0, y0, r, omega, dt, t_max):
+    
+    R_vector = []
+
+    # Create time array
+    t = np.arange(0, t_max, dt)
+
+    for n_object in range(5):
+
+        R = r + n_object * 1
+
+        # Calculate x and y positions
+        x = x0 + R * np.cos(omega * t)
+        y = y0 + R * np.sin(omega * t) 
+
+        # packaging the array for the plotter
+        r_vector = np.asarray((x, y)).T
+
+        R_vector.append(r_vector)
+
+    return pack_object_multi(R_vector)
 
 def olita(x0, y0, omega1, alpha, omega2, dt, t_max):
 
@@ -101,3 +125,31 @@ def kokoro(x0, y0, dt, t_max):
     r_vector = np.asarray((x, y)).T
 
     return pack_object(r_vector)
+
+def kokoro_multi(x0, y0, r, dt, t_max):
+
+    R_vector = []
+
+    # Create time array
+    t = np.arange(0, t_max, dt)
+    
+    for n_object in range(5):
+
+        R = r + n_object * 1
+
+        # Calculate x and y positions
+        x = R * (x0 + 16 * np.sin(t)**3)
+        y = R * (
+            y0 
+            + 13 * np.cos(t) 
+            - 5 * np.cos(2*t) 
+            - 2 * np.cos(3*t) 
+            - np.cos(4*t)
+        )
+
+        # packaging the array for the plotter
+        r_vector = np.asarray((x, y)).T
+
+        R_vector.append(r_vector)
+
+    return pack_object_multi(R_vector)