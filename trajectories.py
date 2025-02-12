8974c51983b7077236561780bbb9abb7a45df75c
MartinAramayo <gabriel.m.aramayo@gmail.com>
2025-02-12 17:03:44 -0300

    ♻ Refractor the code for legibility, reproducibility and add a new trajectory

--- trajectories.py
+++ trajectories.py
@@ -153,3 +153,43 @@ def kokoro_multi(x0, y0, r, dt, t_max):
         R_vector.append(r_vector)
 
     return pack_object_multi(R_vector)
+
+def parabolic_trajectory_multi_kokoro(x0, y0, v0, angle, g, dt):
+    
+    R_vector = []
+
+    
+    # Calculate time of flight
+    t_max = 2 * v0 * np.sin(45) / g
+
+    # Create time array
+    t = np.arange(0, 2*t_max, dt)
+
+    def kokoro_vector_scaler(t):
+        vx = (x0 + 16 * np.sin(t)**3)/12
+        vy = (
+            y0 
+            + 13 * np.cos(t) 
+            - 5 * np.cos(2*t) 
+            - 2 * np.cos(3*t) 
+            - np.cos(4*t)
+        )/12
+        return vx, vy
+    
+    for n_object in range(72):
+    
+        # Convert angle to radians
+        theta = np.radians(angle + 5 * n_object)
+
+        proj_x, proj_y = kokoro_vector_scaler(theta)
+
+        # Calculate x and y positions
+        x = x0 + v0 * proj_x * t
+        y = y0 + v0 * proj_y * t - 0.5 * g * t**2
+
+        # packaging the array for the plotter
+        r_vector = np.asarray((x, y)).T
+
+        R_vector.append(r_vector)
+
+    return pack_object_multi(R_vector)
